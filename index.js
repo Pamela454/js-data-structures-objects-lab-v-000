ecb6c0f3e4d573d396b35387fab4060976b86ab3
Pamela Frieze <pfrieze@gmail.com>
2019-02-05 01:34:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,8 @@ function deleteFromDriverByKey(driver, key, value) {
   return newObj;
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, 
-key, value) {
+function destructivelyDeleteFromDriverByKey(driver, key) {
   delete driver[key];
 
   return driver;
 }
-
